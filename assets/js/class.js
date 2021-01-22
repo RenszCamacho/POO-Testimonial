95d230e3597dd3d9bae3bf099228a900b10831e1
Renszo Camacho <varsovia84@gmail.com>
2021-01-22 12:41:28 +0100

    Class Testimonial. Done.

--- assets/js/class.js
+++ assets/js/class.js
@@ -1,1 +1,9 @@
-
+export default class Testimonial {
+  constructor(image, name, profesion, quote, button) {
+    this.image = image;
+    this.name = name;
+    this.profesion = profesion;
+    this.quote = quote;
+    this.button = button;
+  }
+}
